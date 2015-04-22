06443a51e6ae3ad2a9876edf0d6f91f8f4e000fc
Eugene Vasilchenko <evasilchenko@ordergroove.com>
2015-04-22 19:12:51 -0400

    add initial Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+"use strict"
+grunt.initConfig({
+karma: {
+	options: {
+                configFile: 'karma.conf.js',
+                singleRun: true,
+                autoWatch: false,
+                browsers: ['PhantomJS']
+            },
+	main: {}
+});
+grunt.registerTask('test', function () {
+	grunt.task.run(['karma:main']);
+});
+grunt.registerTask('coverage', ['karma:coverage']);
